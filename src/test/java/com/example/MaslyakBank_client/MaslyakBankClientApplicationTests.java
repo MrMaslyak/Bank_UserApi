72099cbdd786780cca1e1a3b5f14f6776d929170
Ilya Maslyanyi <maslanyjila@gmail.com>
2025-02-02 21:11:28 +0200

    first commit

---
+++ src/test/java/com/example/MaslyakBank_client/MaslyakBankClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MaslyakBank_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaslyakBankClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
